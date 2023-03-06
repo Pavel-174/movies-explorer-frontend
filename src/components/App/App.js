8d45eced82ced2af711f493cba37740b151fa6ab
Pavel Alekseev <pavelbooking.com@yandex.ru>
2023-03-06 13:52:06 +0500

    movies-explorer-frontend

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Register from '../Register/Register';
+import Login from '../Login/Login';
+import Profile from '../Profile/Profile';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
