6b1a49db74c936e6ce2b2b1f2b61334498fa1d31
Pavel Alekseev <pavelbooking.com@yandex.ru>
2023-03-13 11:44:27 +0500

    routes + components Main, Promo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,14 +4,25 @@ import SavedMovies from '../SavedMovies/SavedMovies';
 import Register from '../Register/Register';
 import Login from '../Login/Login';
 import Profile from '../Profile/Profile';
+import Header from '../Header/Header'
 import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+      <div className="App">
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/saved-movies" component={SavedMovies} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/signin" component={Login}/>
+          <Route path="/signup" component={Register} />
+        </Switch>
+      </div>
   );
 }
 
