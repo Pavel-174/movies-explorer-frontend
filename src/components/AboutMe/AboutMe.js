7092ff7a7ed7b9508de8011bac0c154cf86c9fdc
Pavel Alekseev <pavelbooking.com@yandex.ru>
2023-03-14 11:23:21 +0500

    part of Main component (Techs, AboutMe)

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import './AboutMe.css';
+import photo from '../../images/my-photo2.jpg';
 
 const AboutMe = () => {
   return (
-    <section>
+    <section className="about-me">
+      <h2 className="about-me__header">Студент</h2>
+
+      <div className="about-me__container">
+        <div className="about-me__info">
+          <h3 className="about-me__name">Павел</h3>
+          <p className="about-me__job">Фронтенд-разработчик, 29 лет</p>
+          <p className="about-me__description">
+          Я родился и живу в Челябинске, закончил Институт Спорта Туризма и Сервиса Южно-Уральского государственного университета. У меня есть жена и дочь.
+          Я люблю слушать музыку, а ещё увлекаюсь пауэрлтфтингом. 
+          Недавно начал кодить. С 2018 года работал в турфирме «Сусанин». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.
+          </p>
+
+          <ul className="about-me__links">
+            <li><a className="about-me__link" href="https://github.com/Pavel-174">Github</a></li>
+          </ul>
+        </div>
+        <img src={photo} alt="my photo" className="about-me__photo" />
+      </div>
     </section>
   );
 };
--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -3,34 +3,34 @@ import './AboutProject.css';
 
 const AboutProject = () => {
   return (
-    <section className="about" id="about-project">
-      <h2 className="about__header">О проекте</h2>
+    <section className="about-project" id="about-project">
+      <h2 className="about-project__header">О проекте</h2>
 
-      <div className="about__container">
-        <div className="about__info">
-          <h3 className="about__info-title">Дипломный проект включал 5 этапов</h3>
-          <p className="about__info-text">
+      <div className="about-project__container">
+        <div className="about-project__info">
+          <h3 className="about-project__info-title">Дипломный проект включал 5 этапов</h3>
+          <p className="about-project__info-text">
             Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.
           </p>
         </div>
 
-        <div className="about__info">
-          <h3 className="about__info-title">На выполнение диплома ушло 5 недель</h3>
-          <p className="about__info-text">
+        <div className="about-project__info">
+          <h3 className="about-project__info-title">На выполнение диплома ушло 5 недель</h3>
+          <p className="about-project__info-text">
             У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.
           </p>
         </div>
       </div>
 
-      <div className="about__container">
-        <div className="about__time">
-          <h4 className="about__time-title">1 неделя</h4>
-          <p className="about__time-text">Back-end</p>
+      <div className="about-project__container">
+        <div className="about-project__time">
+          <h4 className="about-project__time-title">1 неделя</h4>
+          <p className="about-project__time-text">Back-end</p>
         </div>
 
-        <div className="about__time">
-          <h4 className="about__time-title about__time-title_grey">4 недели</h4>
-          <p className="about__time-text">Front-end</p>
+        <div className="about-project__time">
+          <h4 className="about-project__time-title about-project__time-title_grey">4 недели</h4>
+          <p className="about-project__time-text">Front-end</p>
         </div>
       </div>
     </section>
